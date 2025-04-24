ab8cb437db6514034382af42676af988142dc172
dashkere <sabarkina@gmail.com>
2025-04-24 13:27:58 +0300

    new word

--- testmain.py
+++ testmain.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, World!"}
+    return {"message": "Hello, Wrld!"}
 @app.get("/time")
 def get_server_time():
     return {"server_time": datetime.now().isoformat()}
