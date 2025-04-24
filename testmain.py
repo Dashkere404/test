1f39ffdf2fccf908fb7a52d0d08317be36a0231c
dashkere <sabarkina@gmail.com>
2025-04-24 19:48:37 +0300

    lol

--- testmain.py
+++ testmain.py
@@ -4,6 +4,10 @@ from datetime import datetime
 
 app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
 @app.get("/time")
 def get_server_time():
     return {"server_time": datetime.now().isoformat()}
